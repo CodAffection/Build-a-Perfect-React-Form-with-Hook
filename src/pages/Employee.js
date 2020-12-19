a03dbb1201b7919623746632b79fc10c830240ae
CodAffection <32505654+CodAffection@users.noreply.github.com>
2020-12-19 13:05:28 +0530

    Update Employee.js

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -30,32 +30,12 @@ const modelObject = {
 
 export default function Employee() {
 
-    const validate = (fieldValues = values) => {
-        let temp = { ...errors }
-        if ('fullName' in fieldValues)
-            temp.fullName = fieldValues.fullName ? "" : "This field is required."
-        if ('email' in fieldValues)
-            temp.email = (/$^|.+@.+..+/).test(fieldValues.email) ? "" : "Email is not valid."
-        if ('mobile' in fieldValues)
-            temp.mobile = fieldValues.mobile.length > 9 ? "" : "Minimum 10 numbers required."
-        if ('departmentId' in fieldValues)
-            temp.departmentId = fieldValues.departmentId.length != 0 ? "" : "This field is required."
-        setErrors({
-            ...temp
-        })
-
-        if (fieldValues == values)
-            return Object.values(temp).every(x => x == "")
-    }
-
     const {
         values,
         setValues,
-        errors,
-        setErrors,
         handleInputChange,
         resetForm
-    } = useForm(modelObject, true, validate);
+    } = useForm(modelObject);
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -74,21 +54,18 @@ export default function Employee() {
                         label="Full Name"
                         value={values.fullName}
                         onChange={handleInputChange}
-                        error={errors.fullName}
                     />
                     <Input
                         label="Email"
                         name="email"
                         value={values.email}
                         onChange={handleInputChange}
-                        error={errors.email}
                     />
                     <Input
                         label="Mobile"
                         name="mobile"
                         value={values.mobile}
                         onChange={handleInputChange}
-                        error={errors.mobile}
                     />
                     <Input
                         label="City"
@@ -112,7 +89,6 @@ export default function Employee() {
                         value={values.departmentId}
                         onChange={handleInputChange}
                         options={departmentList}
-                        error={errors.departmentId}
                     />
                     <DatePicker
                         name="hireDate"
